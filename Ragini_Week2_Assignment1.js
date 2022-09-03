331d36d60be1a2093903cfd40da47cc5b97ae460
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-09-03 13:37:25 +0530

    Add files via upload

--- Ragini_Week2_Assignment1.js
+++ Ragini_Week2_Assignment1.js
@@ -1,3 +1,17 @@
-function FtoCConverter(temp) {
-    document.querySelector(".celcius").innerHTML=(temp-32)/1.8;
- }
+
+       let fahrenheit = document.getElementById('fahrenheit');
+       let celsius = document.getElementById('celsius');  
+       
+      
+       fahrenheit.oninput = function () {
+           let c = ((parseFloat(
+               fahrenheit.value) - 32) * 5) / 9;
+           celsius.value = parseFloat(c.toFixed(2));
+ 
+           
+       }
+       celsius.oninput = function () {
+           let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+           fahrenheit.value = parseFloat(f.toFixed(2));
+ 
+       }
