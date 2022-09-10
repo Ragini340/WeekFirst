a565154f4f00e2d165eb898f776f1e16ce9a2caf
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-09-10 18:59:12 +0530

    Add files via upload

---
+++ EXERCISE_QUESTION6.sql
@@ -0,0 +1,4 @@
+	SELECT	 W.LASTNAME "Employee", W.EMPLOYEE_ID "EMP#",
+	         M.LASTNAME "Manager", M.EMPLOYEE_ID "Mgr#"
+	FROM     EMPLOYEES W join EMPLOYEES M
+	ON       (W.MANAGER_ID = M.EMPLOYEE_ID);
